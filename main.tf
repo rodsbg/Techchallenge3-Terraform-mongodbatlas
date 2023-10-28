cfebe46d17d66dc908e2d31ef3fd2fbe62ee5a7f
Rodrigo Goncalves <rodgol@gmail.com>
2023-10-28 14:28:12 -0300

    .

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "mongodbatlas_cluster" "fiap-cluster" {
 
 
 # Outputs to Display
-output "atlas_cluster_connection_string" { value = mongodbatlas_advanced_cluster.atlas-cluster.connection_strings.0.standard_srv }
+output "atlas_cluster_connection_string" { value = mongodbatlas_cluster.fiap-cluster.connection_strings.0.standard_srv }
 output "ip_access_list"    { value = mongodbatlas_project_ip_access_list.ip.ip_address }
 output "project_name"      { value = mongodbatlas_project.atlas-project.name }
 output "username"          { value = mongodbatlas_database_user.db-user.username } 
