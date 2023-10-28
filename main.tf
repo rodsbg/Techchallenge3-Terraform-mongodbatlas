967bdd21cbb82fa130f74435aab2ed8f3a97efd2
Rodrigo Goncalves <rodgol@gmail.com>
2023-10-28 11:57:35 -0300

    new all

--- main.tf
+++ main.tf
@@ -1,14 +1,34 @@
-resource "mongodbatlas_cluster" "cluster-test" {
-  project_id              = "653c23027bd592646a42f993"
-  name                    = "cluster-test-global"
-
-  # Provider Settings "block"
-  provider_name = "AWS"
-  backing_provider_name = "AWS"
-  provider_region_name = "US_EAST_1"
-  provider_instance_size_name = "M0"
+# Create a Project
+resource "mongodbatlas_project" "atlas-project" {
+  org_id = var.atlas_org_id
+  name = var.atlas_project_name
 }
 
+
+# Create a Database User
+resource "mongodbatlas_database_user" "db-user" {
+  username = "user-1"
+  password = random_password.db-user-password.result
+  project_id = mongodbatlas_project.atlas-project.id
+  auth_database_name = "admin"
+  roles {
+    role_name     = "readWrite"
+    database_name = "${var.atlas_project_name}-db"
+  }
+}
+
+# Create a Database Password
+resource "random_password" "db-user-password" {
+  length = 16
+  special = true
+  override_special = "_%@"
+}
+
+# Create Database IP Access List 
+resource "mongodbatlas_project_ip_access_list" "ip" {
+  project_id = mongodbatlas_project.atlas-project.id
+  ip_address = var.ip_address
+}
 
 # Outputs to Display
 output "atlas_cluster_connection_string" { value = mongodbatlas_advanced_cluster.atlas-cluster.connection_strings.0.standard_srv }
@@ -19,5 +39,3 @@ output "user_password"     {
   sensitive = true
   value = mongodbatlas_database_user.db-user.password 
   }
-
-  
